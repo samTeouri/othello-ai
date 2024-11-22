e4497c13cac6fd78483b832e72aeb3de9bd9b9fd
Samrou Teouri <teourisamrou@gmail.com>
2024-11-22 16:22:14 +0100

    Function to filp the captured pieces after a move

--- othello.py
+++ othello.py
@@ -12,10 +12,11 @@ DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 
 PLAYER_TO_MOVE = 'black'
 
 def get_opponent():
+    """Function to get the current opponent"""
     return 'white' if PLAYER_TO_MOVE == 'black' else 'black'
 
-# Function to get all the captured pieces by a move
 def get_captured_pieces(position):
+    """Function to get all the captured pieces by a move"""
     opponent = get_opponent()
     captured_pieces = []
     for dx, dy in DIRECTIONS:
@@ -37,8 +38,8 @@ def get_captured_pieces(position):
                 break
     return captured_pieces
 
-# Function to find all the legal moves that the current player can do
 def find_legal_moves():
+    """Function to find all the legal moves that the current player can do"""
     legal_moves = []
 
     # Iterate through all positions on the game board
@@ -49,13 +50,15 @@ def find_legal_moves():
                 legal_moves.append((n, l))
     return legal_moves
 
-# Function to find if a move is legal
 def is_legal_move(position):
+    """Function to find if a move is legal"""
     legal_moves = find_legal_moves()
     if position in legal_moves:
         return True
     return False
 
-# Function to filp the captured pieces after a move
-#def flip_captured_pieces(position):
-#    captured_pieces = get_captured_pieces(position)
+def flip_captured_pieces(position):
+    """Function to filp the captured pieces after a move"""
+    for e in get_captured_pieces(position):
+        GAMEBOARD[e] = PLAYER_TO_MOVE
+
